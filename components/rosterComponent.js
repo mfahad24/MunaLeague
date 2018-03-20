b224e77fa1baa4660fbc4e753ee9c2c04fbd1991
Mohammed Fahad <mfahad24@users.noreply.github.com>
2018-03-20 15:03:50 -0400

    first commit, nav bar created

---
+++ components/rosterComponent.js
@@ -0,0 +1,11 @@
+(function () {
+var rosterComponent = {
+  templateUrl: `partials/roster.html`,
+  controller: 
+}
+
+angular
+.module("myApp")
+.component("rosterComponent", rosterComponent)
+
+})();
